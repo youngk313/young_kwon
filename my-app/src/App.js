8f3192f118ea2160873810f26fff34a1e56cf3f1
Young <youngkwon313@gmail.com>
2021-01-12 13:48:33 -0800

    Initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello there, <br/>
+          My name is Young. <br/><br/>
+          I am currently a 
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
