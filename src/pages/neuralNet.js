fa78478a8cbafb7ef368b6888c338fa91b5cc418
Young <youngkwon313@gmail.com>
2021-02-18 23:24:28 -0800

    updated neural

--- src/pages/neuralNet.js
+++ src/pages/neuralNet.js
@@ -1,38 +1,57 @@
 import { Component } from 'react';
 
 const RADIUS = 5;
+let X_RAD = Math.PI / 540;
+let Y_RAD = Math.PI / 720;
 
 window.onload = () => {
 	const c = document.getElementById("neural");
-	console.log(c)
-	c.width = window.innerWidth
-	c.height = window.innerHeight
+	c.width = window.innerWidth;
+	c.height = window.innerHeight;
 	const ctx = c.getContext("2d");
+	window.addEventListener('resize', function() {
+		let dimensions = onResize(c);
+		c.width = dimensions[0];
+		c.height = dimensions[1];
+	});
 
 	const START_X = c.width / 2;
 	const START_Y = c.height / 2;
 
-	let SPHERE_R = c.width / 3;
+	let SPHERE_R = c.width / 4;
 	let PERSPECTIVE = c.width * 0.8;
+
 	const neurons = createNeurons(START_X, START_Y, RADIUS, '#01579b', ctx);
-	animate(ctx, c, neurons, SPHERE_R, PERSPECTIVE);
+	animate(ctx, c, neurons, SPHERE_R, PERSPECTIVE, c);
 }
 
-function animate(context, canvas, neurons, sphere_r, persp) {
-	requestAnimationFrame(function() { animate(context, canvas, neurons, sphere_r, persp); });
+
+function onResize() {
+	let dimensions = [window.innerWidth, window.innerHeight];
+	return dimensions;
+}
+
+function animate(context, canvas, neurons, sphere_r, persp, c) {
+
+	requestAnimationFrame(function() { animate(context, canvas, neurons, sphere_r, persp, c); });
 	context.clearRect(0, 0, canvas.width, canvas.height);
 
-	let firstNeuron = neurons[0]
-	firstNeuron.rotate(firstNeuron);
+	let dimensions = onResize(c);
+    c.width = dimensions[0];
+	c.height = dimensions[1];
+
+	let firstNeuron = neurons[0];
 	for (let i = 1; i < neurons.length; ++i) {
-		neurons[i].draw(sphere_r, persp);
+		neurons[i].draw(sphere_r, persp, X_RAD, Y_RAD);
 	}
 	neurons.sort((n1, n2) => {
 		return n1.scale - n2.scale;
 	});
 	for (let i = 1; i < neurons.length; ++i) {
-		neurons[i].draw(sphere_r, persp);
+		neurons[i].draw(sphere_r, persp, X_RAD, Y_RAD);
 	}
+	firstNeuron.rotate(firstNeuron);
+
 }
 
 class Neuron {
@@ -49,7 +68,7 @@ class Neuron {
 		let deltaX = Math.abs(this.x - firstNeuron.x);
 		let deltaY = Math.abs(this.y - firstNeuron.y);
 		let distance = Math.sqrt(Math.pow(deltaX, 2) + Math.pow(deltaY, 2));
-		console.log(distance);
+
 		this.angle += Math.PI / 180;
 		this.x = firstNeuron.x + distance * Math.cos(this.angle);
 		this.y = firstNeuron.y + distance * Math.sin(this.angle);
@@ -91,7 +110,7 @@ class OuterNeuron {
 	project(sphere_r, persp) {
 		this.x = sphere_r * this.dist_ratio * Math.sin(this.phi) * Math.cos(this.theta);
 		this.y = sphere_r * this.dist_ratio * Math.cos(this.phi);
-		this.z = sphere_r * this.dist_ratio *Math.sin(this.phi) * Math.sin(this.theta) + sphere_r;
+		this.z = sphere_r * (this.dist_ratio *Math.sin(this.phi) * Math.sin(this.theta) + 1);
 
 		this.scale = persp / (persp + this.z);
 
@@ -99,20 +118,32 @@ class OuterNeuron {
 		this.yProj = (this.y * this.scale) + this.centreY;
 	}
 
-	rotate() {
-		this.phi -= Math.PI / 360;
-		this.theta += Math.PI / 720;
+	rotate(x_rad, y_rad) {
+		this.phi -= x_rad;
+		this.theta += y_rad;
 	}
 
-	draw(sphere_r, persp) {
-		this.rotate();
+	draw(sphere_r, persp, x_rad, y_rad) {
+		this.rotate(x_rad, y_rad);
 		this.project(sphere_r, persp);
+		this.context.globalAlpha = Math.abs(1 - this.z / (this.centreX * 2));
 		this.context.beginPath();
 		this.context.arc(this.xProj, this.yProj, this.r * this.scale, 0, 2 * Math.PI, true);
 		this.context.fillStyle = this.color;
 		this.context.fill();
 		this.context.stroke();
+
+		/*this.context.lineTo(this.centreX, this.centreY);
+		this.context.strokeStyle = "#7E7E7E";
+		this.context.stroke();*/
 		this.context.closePath();
+	}
+}
+
+class Connection {
+	constructor(firstNeuron) {
+		this.startX = firstNeuron.x;
+		this.startY = firstNeuron.y; 
 	}
 }
 
@@ -120,27 +151,11 @@ function createNeurons(startx, starty, radius, color, context) {
 	let neurons = [];
 	let centreNeuron = new Neuron(startx, starty, radius, 'orange', context);
 	neurons.push(centreNeuron);
-	for (let i = 0; i < 200; ++i)
+	for (let i = 0; i < 150; ++i)
 		neurons.push(new OuterNeuron(centreNeuron, context, radius, color));
 	return neurons;
 }
 
-function drawCircle(context, x, y, r) {
-	context.lineWidth = 0.5;
-	context.beginPath();
-	context.arc(x, y, r, 0, 2*Math.PI, true);
-	context.fillStyle = 'orange';
-	context.fill();
-	context.stroke();
-	context.closePath();
-}
-
-function drawLink(context, x, y) {
-	context.strokeStyle = "#FFFFFF";
-	context.lineWidth = 2;
-	context.lineTo(x, y);
-	context.stroke();	
-}
 
 class Neural extends Component {
     render() {
